fe1032349da0eb34a51a62a65dac3dfc5914cc9b
Mattia Corradi <mattia.corradi.tr@gmail.com>
2025-11-05 00:06:48 +0100

    update sha

--- Casks/cfgen.rb
+++ Casks/cfgen.rb
@@ -1,12 +1,12 @@
 cask "cfgen" do
   version "0.1.0"
-  sha256 "c3a5b745258520186ecdda7f89385074bd43d6c85dfd7d56165fb0924337e759"
+  sha256 "a9619fc00378d716c6be72b7169feee029eb5da12b1c161d0b710e59b83c609c"
   url "https://github.com/astante-dev/cfgen/releases/download/v#{version}/CFGen.app.zip"
   name "CFGen"
   homepage "https://github.com/astante-dev/cfgen"
   app "CFGen.app"
   zap trash: [
-    "~/Library/Application Support/com.example.rcf",
-    "~/Library/Preferences/com.eaxmple.rcf.plist",
+    "~/Library/Application Support/it.astante.rcf",
+    "~/Library/Preferences/it.astante.rcf.plist",
   ]
 end
