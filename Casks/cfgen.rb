df1be37371b995a16795d1d1943b89294069d7d7
Mattia Corradi <mattia.corradi.tr@gmail.com>
2025-11-06 00:11:53 +0100

    Fix sha256 checksum for cfgen version 1.1.0

--- Casks/cfgen.rb
+++ Casks/cfgen.rb
@@ -1,6 +1,6 @@
 cask "cfgen" do
   version "1.1.0"
-  sha256 "sha256:7121c1eb74e93efb3ca38ff2f2b234d92d7bf652e63b14f364ca4a7fe833b4a7"
+  sha256 "7121c1eb74e93efb3ca38ff2f2b234d92d7bf652e63b14f364ca4a7fe833b4a7"
   url "https://github.com/astante-dev/cfgen/releases/download/v#{version}/CFGen.zip"
   name "CFGen"
   homepage "https://github.com/astante-dev/cfgen"
